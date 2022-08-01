a8cd306f3aeb94f36c8f429616e08b870e87073b
ViktoriaKrent <mrs.victoria.krent.99@gmail.com>
2022-08-01 14:43:15 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Number([...(-1 * n).toString()].reverse().join(""));
+    }
+    return Number([...n.toString()].reverse().join(""));
+};
